467482aa1911e8a3df8a392c095df948746783c7
wladid <wladid@669a04be-7fdf-a541-be86-3aa31e907dbd>
2011-01-21 11:29:22 +0000

    Examples adjusted to the changes in Revision 85
    git-svn-id: https://accessquery.svn.sourceforge.net/svnroot/accessquery@86 669a04be-7fdf-a541-be86-3aa31e907dbd

--- Examples/NotificationWindows/MyNotificationWindow.pas
+++ Examples/NotificationWindows/MyNotificationWindow.pas
@@ -9,33 +9,11 @@ uses
 type
 	TMyNotificationWindow = class(TNotificationWindow)
 		Label1:TLabel;
-	private
-		FCloseTimeout:Integer;
-
-		procedure SetCloseTimeout(CloseTimeout:Integer);
-	public
-
-		property CloseTimeout:Integer read FCloseTimeout write SetCloseTimeout;
 	end;
 
 implementation
 
 {$R *.dfm}
 
-procedure TMyNotificationWindow.SetCloseTimeout(CloseTimeout:Integer);
-begin
-	if CloseTimeout = FCloseTimeout then
-		Exit;
-	FCloseTimeout:=CloseTimeout;
-	Take(Self)
-		.CancelDelays(778)
-		.IfThen(CloseTimeout > 0)
-		.EachDelay(CloseTimeout,
-			function(AQ:TAQ; O:TObject):Boolean
-			begin
-				Close;
-				Result:=TRUE;
-			end, 778);
-end;
 
 end.
