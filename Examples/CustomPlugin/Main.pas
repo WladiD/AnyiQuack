f1e34e81b1a2e92572c250245bfdc27774db08d6
wladid <mail@wladid.de>
2014-10-24 23:23:53 +0200

    Code style adjustments in CustomPlugin example

--- Examples/CustomPlugin/Main.pas
+++ Examples/CustomPlugin/Main.pas
@@ -3,33 +3,31 @@ unit Main;
 interface
 
 uses
-	Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-	Dialogs, StdCtrls, AnyiQuack, AQPCustomPlugin;
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, AnyiQuack, AQPCustomPlugin;
 
 type
-	TForm1 = class(TForm)
-		Button1: TButton;
-		Button2: TButton;
-		Button3: TButton;
-		Button4: TButton;
-		Button5: TButton;
-		Button6: TButton;
-		Button7: TButton;
-		Button8: TButton;
-		Button9: TButton;
-		Button10: TButton;
-		HideButton: TButton;
-		ShowButton: TButton;
-		procedure ShowButtonClick(Sender: TObject);
-		procedure HideButtonClick(Sender: TObject);
-	private
-		function TargetButtons:TAQ;
-	public
-		{ Public-Deklarationen }
-	end;
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Button10: TButton;
+    HideButton: TButton;
+    ShowButton: TButton;
+    procedure ShowButtonClick(Sender: TObject);
+    procedure HideButtonClick(Sender: TObject);
+  private
+    function TargetButtons:TAQ;
+  end;
 
 var
-	Form1: TForm1;
+  Form1: TForm1;
 
 implementation
 
@@ -37,24 +35,24 @@ implementation
 
 procedure TForm1.HideButtonClick(Sender: TObject);
 begin
-	TargetButtons
-		.Plugin<TAQPCustomPlugin>
-		.Hide;
+  TargetButtons
+    .Plugin<TAQPCustomPlugin>
+    .Hide;
 end;
 
 procedure TForm1.ShowButtonClick(Sender: TObject);
 begin
-	TargetButtons
-		.Plugin<TAQPCustomPlugin>
-		.Show;
+  TargetButtons
+    .Plugin<TAQPCustomPlugin>
+    .Show;
 end;
 
 function TForm1.TargetButtons:TAQ;
 begin
-	Result:=Take(Form1)
-		.ChildrenChain
-		.FilterChain(TButton)
-		.ExcludeChain(OA([HideButton, ShowButton]));
+  Result := Take(Form1)
+    .ChildrenChain
+    .FilterChain(TButton)
+    .ExcludeChain(OA([HideButton, ShowButton]));
 end;
 
 end.
