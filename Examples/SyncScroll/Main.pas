2dd19b1058b92b04af89691ee2795f834e4467f9
wladid <mail@wladid.de>
2014-10-25 00:08:08 +0200

    Adjustment of the coding style

--- Examples/SyncScroll/Main.pas
+++ Examples/SyncScroll/Main.pas
@@ -3,38 +3,37 @@ unit Main;
 interface
 
 uses
-	Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-	Dialogs, StdCtrls, ExtCtrls, ComCtrls, Math, AnyiQuack;
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls, ComCtrls, Math, AnyiQuack;
 
 type
-	TForm1 = class(TForm)
-		ListBox1:TListBox;
-		ListBox2:TListBox;
-		ListBox3:TListBox;
-		ListBox4:TListBox;
-		Panel1:TPanel;
-		Panel2:TPanel;
-		SyncScrollBar: TScrollBar;
-		ItemsCountTrackBar: TTrackBar;
-		Label1:TLabel;
-		Label2:TLabel;
-		AnimatedScrollCheckBox:TCheckBox;
-		Label3:TLabel;
-		AnimationStyleComboBox:TComboBox;
-		Panel3:TPanel;
-		Label4:TLabel;
-		AnimationDurationComboBox:TComboBox;
-		procedure ItemsCountTrackBarChange(Sender:TObject);
-		procedure FormCreate(Sender:TObject);
-		procedure SyncScrollBarChange(Sender:TObject);
-	private
-		function ListBoxesAQ:TAQ;
-	public
-		{Public-Deklarationen}
-	end;
+  TForm1 = class(TForm)
+    ListBox1: TListBox;
+    ListBox2: TListBox;
+    ListBox3: TListBox;
+    ListBox4: TListBox;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    SyncScrollBar: TScrollBar;
+    ItemsCountTrackBar: TTrackBar;
+    Label1: TLabel;
+    Label2: TLabel;
+    AnimatedScrollCheckBox: TCheckBox;
+    Label3: TLabel;
+    AnimationStyleComboBox: TComboBox;
+    Panel3: TPanel;
+    Label4: TLabel;
+    AnimationDurationComboBox: TComboBox;
+
+    procedure ItemsCountTrackBarChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure SyncScrollBarChange(Sender: TObject);
+  private
+    function ListBoxesAQ: TAQ;
+  end;
 
 var
-	Form1:TForm1;
+  Form1:TForm1;
 
 implementation
 
@@ -42,86 +41,86 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-	ItemsCountTrackBarChange(ItemsCountTrackBar);
+  ItemsCountTrackBarChange(ItemsCountTrackBar);
 end;
 
 procedure TForm1.ItemsCountTrackBarChange(Sender: TObject);
 begin
-	TAQ.Take(Sender)
-		.CancelDelays
-		.EachDelay(500,
-			function(AQ:TAQ; O:TObject):Boolean
-			begin
-				Result:=TRUE;
-				SyncScrollBar.SetParams(0, 0, TTrackBar(Sender).Position - 1);
-				ListBoxesAQ
-					.Each(
-						function(AQ:TAQ; O:TObject):Boolean
-						begin
-							Result:=TRUE;
-							with TListBox(O) do
-							begin
-								Clear;
-								Items.BeginUpdate
-							end;
-						end)
-					.EachRepeat(TTrackBar(O).Position,
-						function(AQ:TAQ; O:TObject):Boolean
-						begin
-							Result:=TRUE;
-							with TListBox(O) do
-								Items.Add(Format('Item #%d', [Items.Count + 1]));
-						end)
-					.Each(
-						function(AQ:TAQ; O:TObject):Boolean
-						begin
-							Result:=TRUE;
-							TListBox(O).Items.EndUpdate;
-						end);
-			end);
+  TAQ.Take(Sender)
+    .CancelDelays
+    .EachDelay(500,
+      function(AQ: TAQ; O: TObject): Boolean
+      begin
+        Result := TRUE;
+        SyncScrollBar.SetParams(0, 0, TTrackBar(Sender).Position - 1);
+        ListBoxesAQ
+          .Each(
+            function(AQ: TAQ; O: TObject): Boolean
+            begin
+              Result := TRUE;
+              with TListBox(O) do
+              begin
+                Clear;
+                Items.BeginUpdate
+              end;
+            end)
+          .EachRepeat(TTrackBar(O).Position,
+            function(AQ: TAQ; O: TObject): Boolean
+            begin
+              Result := TRUE;
+              with TListBox(O) do
+                Items.Add(Format('Item #%d', [Items.Count + 1]));
+            end)
+          .Each(
+            function(AQ: TAQ; O: TObject): Boolean
+            begin
+              Result := TRUE;
+              TListBox(O).Items.EndUpdate;
+            end);
+      end);
 end;
 
-function TForm1.ListBoxesAQ:TAQ;
+function TForm1.ListBoxesAQ: TAQ;
 begin
-	Result:=Take(Form1)
-		.ChildrenChain(TRUE)
-		.FilterChain(TListBox);
+  Result := Take(Form1)
+    .ChildrenChain(TRUE)
+    .FilterChain(TListBox);
 end;
 
 procedure TForm1.SyncScrollBarChange(Sender:TObject);
 var
-	FirstItemIndex, ScrollItemIndex:Integer;
-	ScrollEach:TEachFunction;
+  FirstItemIndex, ScrollItemIndex: Integer;
+  ScrollEach: TEachFunction;
 begin
-	FirstItemIndex:=ListBox1.TopIndex;
-	ScrollItemIndex:=SyncScrollBar.Position;
-	Form1.Caption:=IntToStr(ScrollItemIndex);
+  FirstItemIndex := ListBox1.TopIndex;
+  ScrollItemIndex := SyncScrollBar.Position;
+  Form1.Caption := IntToStr(ScrollItemIndex);
 
-	ScrollEach:=function(AQ:TAQ; O:TObject):Boolean
-	begin
-		Result:=TRUE;
-		if not (O is TListBox) then
-			Exit;
-		{**
-		 * Animiert
-		 *}
-		if Assigned(AQ.CurrentInterval) then
-			TListBox(O).TopIndex:=TAQ.EaseInteger(FirstItemIndex, ScrollItemIndex,
-				AQ.CurrentInterval.Progress, TEaseType(Ord(AnimationStyleComboBox.ItemIndex)))
-		{**
-		 * Sofort
-		 *}
-		else
-			TListBox(O).TopIndex:=ScrollItemIndex;
-	end;
+  ScrollEach := function(AQ: TAQ; O: TObject): Boolean
+  begin
+    Result := TRUE;
+    if not (O is TListBox) then
+      Exit;
+    {**
+     * Animiert
+     *}
+    if Assigned(AQ.CurrentInterval) then
+      TListBox(O).TopIndex := TAQ.EaseInteger(FirstItemIndex, ScrollItemIndex,
+        AQ.CurrentInterval.Progress, TEaseType(Ord(AnimationStyleComboBox.ItemIndex)))
+    {**
+     * Sofort
+     *}
+    else
+      TListBox(O).TopIndex := ScrollItemIndex;
+  end;
 
-	ListBoxesAQ
-		.IfThen(AnimatedScrollCheckBox.Checked)
-			.CancelTimers
-			.EachTimer((AnimationDurationComboBox.ItemIndex + 1) * 100, ScrollEach)
-		.IfElse
-			.Each(ScrollEach)
-		.IfEnd;
+  ListBoxesAQ
+    .IfThen(AnimatedScrollCheckBox.Checked)
+      .CancelTimers
+      .EachTimer((AnimationDurationComboBox.ItemIndex + 1) * 100, ScrollEach)
+    .IfElse
+      .Each(ScrollEach)
+    .IfEnd;
 end;
 
 end.
