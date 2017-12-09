e4009e5e882ec642425b98ca60d6bb31da471d29
WladiD <furevest@gmail.com>
2017-12-09 09:44:37 +0100

    _PackExamples.bat uses now 7zip

--- _PackExamples.bat
+++ _PackExamples.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd Examples
-SET Pack="C:\Program Files (x86)\WinRAR\winrar.exe" U -afzip -ep -m5 AnyiQuack.Examples.zip 
+SET Pack="C:\Program Files\7-Zip\7z.exe" u -tzip -m9 AnyiQuack.Examples.zip 
 %Pack% AnimatedAlign\AnimatedAlign.exe
 %Pack% EasingSuite\EasingSuite.exe
 %Pack% PulsateEdits\PulsateEdits.exe
