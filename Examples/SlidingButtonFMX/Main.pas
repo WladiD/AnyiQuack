b1307000d8aa3c75c72ca41715a2147c056d81bb
WladiD <furevest@gmail.com>
2019-07-12 14:16:09 +0200

    The SlidingButtonFMX sample contains now for each ease type a button

--- Examples/SlidingButtonFMX/Main.pas
+++ Examples/SlidingButtonFMX/Main.pas
@@ -21,13 +21,25 @@ uses
 
 type
   TMainForm = class(TForm)
-    Button1: TButton;
+    QuintSlideButton: TButton;
     Label1: TLabel;
     XTrackBar: TTrackBar;
     YTrackBar: TTrackBar;
     Label2: TLabel;
     Label3: TLabel;
-    procedure Button1Click(Sender: TObject);
+    LinearSlideButton: TButton;
+    CubicSlideButton: TButton;
+    QuartSlideButton: TButton;
+    SextSlideButton: TButton;
+    QuadSlideButton: TButton;
+    ElasticSlideButton: TButton;
+    SinusSlideButton: TButton;
+    BackSlideButton: TButton;
+    MiddleWaveSlideButton: TButton;
+    LowWaveSlideButton: TButton;
+    HighWaveButton: TButton;
+    procedure QuintSlideButtonClick(Sender: TObject);
+    procedure TrackBarDblClick(Sender: TObject);
   end;
 
 var
@@ -37,38 +49,58 @@ implementation
 
 {$R *.fmx}
 
-procedure TMainForm.Button1Click(Sender: TObject);
+procedure TMainForm.QuintSlideButtonClick(Sender: TObject);
 var
-  TypesAniPlugin: TAQPSystemTypesAnimations;
-  NewLeft, NewTop: Single;
   ButtonSender: TButton absolute Sender;
 begin
-  TypesAniPlugin := Take(Sender)
+  Take(Self)
+    .ChildrenChain
+    .FilterChain(TButton)
     .CancelAnimations
-    .Plugin<TAQPSystemTypesAnimations>;
+    .Each(
+      function(AQ: TAQ; O: TObject): Boolean
+      var
+        TypesAniPlugin: TAQPSystemTypesAnimations;
+        NewLeft, NewTop: Single;
+      begin
+        Result := True;
+        TypesAniPlugin := Take(O).Plugin<TAQPSystemTypesAnimations>;
 
-  NewLeft := Random(Trunc(ClientWidth - ButtonSender.Width));
-  NewTop := Random(Trunc(ClientHeight - ButtonSender.Height));
+        NewLeft := Random(Trunc(ClientWidth - ButtonSender.Width));
+        NewTop := Random(Trunc(ClientHeight - ButtonSender.Height));
 
-  TypesAniPlugin.SingleAnimation(NewLeft,
-    function(RefObject: TObject): Single
-    begin
-      Result := TButton(RefObject).Position.X;
-    end,
-    procedure(RefObject: TObject; const NewLeft: Single)
-    begin
-      TButton(RefObject).Position.X := NewLeft;
-    end, Trunc(XTrackBar.Value), 0, TAQ.Ease(etBack));
+        TypesAniPlugin.SingleAnimation(NewLeft,
+          function(RefObject: TObject): Single
+          begin
+            Result := TButton(RefObject).Position.X;
+          end,
+          procedure(RefObject: TObject; const NewLeft: Single)
+          begin
+            TButton(RefObject).Position.X := NewLeft;
+          end, Trunc(XTrackBar.Value), 0, TAQ.Ease(TEaseType(ButtonSender.Tag))); // Random ease type: TAQ.Ease(TEaseType(Random(Ord(High(TEaseType)))))
 
-  TypesAniPlugin.SingleAnimation(NewTop,
-    function(RefObject: TObject): Single
-    begin
-      Result := TButton(RefObject).Position.Y;
-    end,
-    procedure(RefObject: TObject; const NewLeft: Single)
-    begin
-      TButton(RefObject).Position.Y := NewLeft;
-    end, Trunc(YTrackBar.Value), 0, TAQ.Ease(etElastic));
+        TypesAniPlugin.SingleAnimation(NewTop,
+          function(RefObject: TObject): Single
+          begin
+            Result := TButton(RefObject).Position.Y;
+          end,
+          procedure(RefObject: TObject; const NewLeft: Single)
+          begin
+            TButton(RefObject).Position.Y := NewLeft;
+          end, Trunc(YTrackBar.Value), 0, TAQ.Ease(TEaseType(ButtonSender.Tag)));
+      end);
+end;
+
+procedure TMainForm.TrackBarDblClick(Sender: TObject);
+var
+  SourceTB: TTrackBar absolute Sender;
+  TargetTB: TTrackBar;
+begin
+  if SourceTB = XTrackBar then
+    TargetTB := YTrackBar
+  else
+    TargetTB := XTrackBar;
+  TargetTB.Value := SourceTB.Value;
 end;
 
 end.
