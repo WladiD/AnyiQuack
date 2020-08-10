af00136b42431dd8ad456d4a4cd524759f884fcd
John Kouraklis <john.kouraklis@nusenvision.com>
2020-08-10 23:25:22 +0100

    + Time elapsed label in FMX

--- Examples/NotificationWindowsFMX/MyNotificationWindow.pas
+++ Examples/NotificationWindowsFMX/MyNotificationWindow.pas
@@ -3,7 +3,7 @@ unit MyNotificationWindow;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
   Notifications.FMX.Base, FMX.Controls.Presentation;
 
@@ -12,11 +12,13 @@ type
     lbTitle: TLabel;
     btnAction: TButton;
     pbrAction: TProgressBar;
+    Label1: TLabel;
     procedure FormCreate(Sender: TObject);
     procedure btnActionClick(Sender: TObject);
   private
-    { Private declarations }
+
   protected
+    procedure OnAutoCloseCountDown(const ElapsedTimeMs: Real); override;
     function AutoClosePossible: Boolean; override;
   public
     { Public declarations }
@@ -27,12 +29,21 @@ implementation
 uses
   AnyiQuack;
 
+const
+  TIME_ELAPSED_STR = 'Time to Close: %5f';
+
 {$R *.fmx}
 
 procedure TMyNotificationWindowFMX.FormCreate(Sender: TObject);
 begin
   inherited;
   pbrAction.Visible:=false;
+end;
+
+procedure TMyNotificationWindowFMX.OnAutoCloseCountDown(
+  const ElapsedTimeMs: Real);
+begin
+  Label1.Text:=format(TIME_ELAPSED_STR, [ElapsedTimeMs]);
 end;
 
 { TMyNotificationWindowFMX }
